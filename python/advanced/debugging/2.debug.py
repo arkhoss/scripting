a9418141212bd9053e80dee607c4af1e00e9e8e5
David Caballero <d@dcaballero.net>
2019-08-24 18:09:36 -0500

    advanced chapters completed

---
+++ python/advanced/debugging/2.debug.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+import pdb
+
+print('Statement 1')
+for i in range(5):
+    print('Statement ' + str(i+2))
+print('Statement 7')
+pdb.pm()
---
+++ python/advanced/debugging/Test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def Test(a,b):
+    sum = 0;
+    for i in range(b):
+        sum += i * a
+    return sum
+ 
