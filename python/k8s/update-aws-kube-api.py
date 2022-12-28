6c23c02bd86bf700120069373754ee094d55e2f6
David Caballero <d@dcaballero.net>
2022-12-28 03:19:28 +0000

    script to update kubeconfig for aws clusters

---
+++ python/k8s/update-aws-kube-api.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+# Author: David Caballero <d@dcaballero.net>
+# Version: 1.0
+# Description: update api versions of kubeconfig
+# Prerequisites:
+#   Python 3.9+
+#   pip3 install docopt
+# !!!IMPORTANT make a backup of your kubeconfig file from ~/.kube/config
+# before running this script!
+
+"""
+
+Usage:
+  update-aws-kube-api.py [--path=<p>] [--debug] [--dry-run]
+  update-aws-kube-api.py -h | --help | --version
+
+Options:
+  -h --help            Show this screen.
+  -v --version         Show version.
+  -p --path=<p>        Kubeconfig file path
+  -r --debug           Debug mode, default false.
+  -d --dry-run         Run without remove Runners, default false.
+
+"""
+
+from docopt import docopt
+from pathlib import Path
+import subprocess, os, sys, time, yaml
+
+def load_yaml(filename):
+    """
+    """
+    try:
+        print("Loading yaml file")
+        with open(filename, "r") as read_file:
+            loaded_file = yaml.safe_load(read_file)
+            print("Yaml file loaded")
+            return loaded_file
+    except Exception as errors:
+        raise errors
+
+def kube_update(context,alias,profile):
+    """
+    """
+    try:
+        region = context["context"]["cluster"][12:21]
+        cluster_name = context["context"]["cluster"].partition('/')[2]
+        print("Updating cluster: " + cluster_name)
+        update_result = subprocess.call(
+            "aws eks --region " + region + " update-kubeconfig --name " + cluster_name  + " --alias " + alias + " --profile " + profile,
+        shell=True,
+        )
+        print("Result update: " + str(update_result))
+        if  update_result != 0:
+            print("Cluster not updated... skipped")
+            return "ClusterNotUpdated"
+        return update_result
+    except subprocess.CalledProcessError as errors:
+        raise errors
+
+def find_profile(cluster_arn, kubeconfig):
+    """
+    """
+    try:
+        users = kubeconfig["users"]
+        user_name = ''
+        profile = ''
+        for i, user in enumerate(users):
+            user_name = users[i]["name"]
+            if user_name == cluster_arn:
+                 value_profile = users[i]["user"]["exec"]["env"][0]["value"]
+                 profile = value_profile
+        return profile
+    except Exception as errors:
+        raise errors
+
+def main(arguments):
+    home_path = str(Path.home())
+    filename = os.path.join(home_path, ".kube/config")
+    #print(load_yaml(filename))
+
+    kubeconfig = load_yaml(filename)
+
+    contexts = kubeconfig["contexts"]
+
+    alias = ''
+
+    for i, context in enumerate(contexts):
+        print("Context" + str(i))
+        #print("Context content" + str(context) )
+        alias = context["name"]
+        cluster_arn = context["context"]["cluster"]
+        if "aws" in cluster_arn:
+            print("Updating Alias: " + alias )
+            #kube_update(context,alias)
+            profile = find_profile(cluster_arn,kubeconfig)
+            print("profile:" + str(profile))
+            update_result = kube_update(context,alias,profile)
+            print("Update: " + str(update_result))
+        else:
+            print("Not an AWS Cluster... skipped")
+
+
+
+if __name__ == '__main__':
+    arguments                = docopt(__doc__, version='1.0.0')
+    dry_run                  = arguments['--dry-run']
+    debug                    = arguments['--debug']
+    path                     = arguments['--path']
+    main(arguments)
